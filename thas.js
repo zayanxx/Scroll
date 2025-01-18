57029ca8975c6bfd8f6bbc4d76964cc00bf49210
zayanxx <smiliesanjay@gmail.com>
2025-01-18 22:16:13 +0530

    Scroll

---
+++ thas.js
@@ -0,0 +1,18 @@
+const ScrollRevealOption = {
+    duration : 1000,
+    ditance : "50px",
+    origin : "bottom",
+};
+
+ScrollReveal().reveal("header .letter-t",{
+    duration : 1000,
+    Delay : 1500,
+});
+ScrollReveal().reveal("header .exlpore-more",{
+    duration : 1000,
+    Delay : 1500,
+});
+ScrollReveal().reveal("header .my-events",{
+    duration : 1000,
+    Delay : 1500,
+});
